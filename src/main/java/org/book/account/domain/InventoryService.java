2cb7c94dbfb216a7d098e515a0bbbda8e6a9c42b
axelMp <axel.krauth@methodpark.de>
2014-04-13 09:07:33 +0200

    Track todos in ticketing system

--- src/main/java/org/book/account/domain/InventoryService.java
+++ src/main/java/org/book/account/domain/InventoryService.java
@@ -5,11 +5,11 @@ public class InventoryService {
     /**
      * Adds transactions such that sum of account closures for all accounts attached to the physical account
      * equals currentlyInPhysicalAccount
-     * @param account account to take stock of
+     *
+     * @param account            account to take stock of
      * @param currentlyInAccount current value of physical account
-     * @param inventoryAccount account to book any difference between current value and book value against
+     * @param inventoryAccount   account to book any difference between current value and book value against
      */
     public void takeStock(PhysicalAccount account, Amount currentlyInAccount, Account inventoryAccount) {
-        // TODO
     }
 }
