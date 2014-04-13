e1cf8c36e4edd11b100761282b83694b22bef987
axelMp <axel.krauth@methodpark.de>
2014-04-13 08:17:50 +0200

    Consumers may want to use persistence features

--- src/main/java/org/book/account/domain/Amount.java
+++ src/main/java/org/book/account/domain/Amount.java
@@ -1,39 +1,43 @@
 package org.book.account.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 /**
  * Value class of an amount of money
  */
+@Embeddable
 public class Amount {
-    public enum Currency {
-        EUR,
-        USD
-    }
-
-    public static Amount noAmount() {
-        return new Amount(0,Currency.EUR);
-    }
-
-    public static Amount add(Amount anAmount,Amount anotherAmount ) {
-        if ( ! anAmount.getCurrency().equals(anotherAmount.getCurrency()))   {
-            throw new IllegalArgumentException("cannot sum amounts of different currencies ("+anAmount.getCurrency()+" and "+anotherAmount.getCurrency()+")");
-        }
-        return new Amount(anAmount.getCents()+anotherAmount.getCents(),anAmount.getCurrency());
-    }
-
-    public static Amount subtract(Amount anAmount, Amount anotherAmount) {
-        if ( ! anAmount.getCurrency().equals(anotherAmount.getCurrency()))   {
-            throw new IllegalArgumentException("cannot sum amounts of different currencies ("+anAmount.getCurrency()+" and "+anotherAmount.getCurrency()+")");
-        }
-        return new Amount(anAmount.getCents()-anotherAmount.getCents(),anAmount.getCurrency());
-    }
+    @Column(name = "CENTS")
+    private final Integer cents;
+    @Column(name = "CURRENCY")
+    @Enumerated(EnumType.STRING)
+    private final Currency currency;
 
     public Amount(Integer cents, Currency currency) {
         this.cents = cents;
         this.currency = currency;
     }
 
-    private final Integer cents;
-    private final Currency currency;
+    public static Amount noAmount() {
+        return new Amount(0, Currency.EUR);
+    }
+
+    public static Amount add(Amount anAmount, Amount anotherAmount) {
+        if (!anAmount.getCurrency().equals(anotherAmount.getCurrency())) {
+            throw new IllegalArgumentException("cannot sum amounts of different currencies (" + anAmount.getCurrency() + " and " + anotherAmount.getCurrency() + ")");
+        }
+        return new Amount(anAmount.getCents() + anotherAmount.getCents(), anAmount.getCurrency());
+    }
+
+    public static Amount subtract(Amount anAmount, Amount anotherAmount) {
+        if (!anAmount.getCurrency().equals(anotherAmount.getCurrency())) {
+            throw new IllegalArgumentException("cannot sum amounts of different currencies (" + anAmount.getCurrency() + " and " + anotherAmount.getCurrency() + ")");
+        }
+        return new Amount(anAmount.getCents() - anotherAmount.getCents(), anAmount.getCurrency());
+    }
 
     public Integer getCents() {
         return cents;
@@ -41,5 +45,10 @@ public class Amount {
 
     public Currency getCurrency() {
         return currency;
+    }
+
+    public enum Currency {
+        EUR,
+        USD
     }
 }
