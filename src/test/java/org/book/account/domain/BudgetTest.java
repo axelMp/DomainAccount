cad3257fdc6fcf4e835ef9b4d3d7d2852d53e083
axelMp <axel.krauth@methodpark.de>
2014-04-19 10:04:16 +0200

    Test assumption explicitly

---
+++ src/test/java/org/book/account/domain/BudgetTest.java
@@ -0,0 +1,21 @@
+package org.book.account.domain;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+public class BudgetTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void transactionIsPlannedToEndBeforeItStarted_IllegalArgumentException() {
+        Ledger ledger = new Ledger("randomName");
+        Budget sut = new Budget(ledger);
+        Amount randomAmount = Utilities.generateRandomAmountInEuro();
+        Account randomAccount = Utilities.generateRandomAccount(ledger);
+        Account anotherRandomAccount = Utilities.generateRandomAccount(ledger);
+
+        Date randomDate = Utilities.today();
+        Date dayBeforeRandomDate = Utilities.previousDay(randomDate);
+
+        sut.plan("randomString", randomDate, dayBeforeRandomDate, randomAmount, randomAccount, anotherRandomAccount, true);
+    }
+}
