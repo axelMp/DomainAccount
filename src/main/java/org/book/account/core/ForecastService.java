3e526acaa0497e440c6823b4bce1ff4a310b60e5
axelMp <axel.krauth@methodpark.de>
2014-04-20 18:40:04 +0200

    Simplify even more

--- src/main/java/org/book/account/core/ForecastService.java
+++ src/main/java/org/book/account/core/ForecastService.java
@@ -7,38 +7,26 @@ import org.book.account.domain.ITransaction;
 
 import java.util.Date;
 import java.util.List;
-import java.util.ListIterator;
 
 class ForecastService {
     Amount forecastClosure(IAccount anAccount, Date forecastOn) {
         List<IPlannedTransaction> plannedTransactions = anAccount.getPlannedTransactions();
         List<ITransaction> transactions = anAccount.getTransactions();
-        ListIterator<IPlannedTransaction> iterator = plannedTransactions.listIterator();
         Date today = new Date();
-        while (iterator.hasNext()) {
-            IPlannedTransaction plannedTransaction1 = iterator.next();
-            if (plannedTransaction1.matchesAnyPerformedTransaction(transactions)) {
-                iterator.remove();
-            }
-        }
-
         Amount currentClosure = anAccount.closure(today);
-        Amount expectedClosure = sumPlannedTransactions(plannedTransactions, anAccount, forecastOn);
-        return Amount.add(currentClosure, expectedClosure);
-    }
+        Amount expectedClosure = Amount.noAmount();
 
-    private Amount sumPlannedTransactions(List<IPlannedTransaction> plan, IAccount anAccount, Date forecastOn) {
-        Amount sum = Amount.noAmount();
-        Date today = new Date();
-        for (IPlannedTransaction plannedTransaction : plan) {
-            Amount forecastOfPlannedTransaction = plannedTransaction.forecast(today, forecastOn);
+        for (IPlannedTransaction plannedTransaction : plannedTransactions) {
+            if (!plannedTransaction.matchesAnyPerformedTransaction(transactions)) {
+                Amount forecastOfPlannedTransaction = plannedTransaction.forecast(today, forecastOn);
 
-            if (anAccount.equals(plannedTransaction.getCreditor())) {
-                sum = Amount.add(sum, forecastOfPlannedTransaction);
-            } else {
-                sum = Amount.subtract(sum, forecastOfPlannedTransaction);
+                if (anAccount.equals(plannedTransaction.getCreditor())) {
+                    expectedClosure = Amount.add(expectedClosure, forecastOfPlannedTransaction);
+                } else {
+                    expectedClosure = Amount.subtract(expectedClosure, forecastOfPlannedTransaction);
+                }
             }
         }
-        return sum;
+        return Amount.add(currentClosure, expectedClosure);
     }
 }
