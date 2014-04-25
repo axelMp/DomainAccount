67a655c45b60cab7e64d6739d7bef5f1d06d52e2
axelMp <axel.krauth@methodpark.de>
2014-04-25 23:10:13 +0200

    allow for modification of entity

--- src/main/java/org/book/account/core/PlannedTransaction.java
+++ src/main/java/org/book/account/core/PlannedTransaction.java
@@ -1,10 +1,7 @@
 package org.book.account.core;
 
 import org.apache.commons.lang3.Validate;
-import org.book.account.domain.Amount;
-import org.book.account.domain.IPlannedTransaction;
-import org.book.account.domain.Period;
-import org.book.account.domain.Schedule;
+import org.book.account.domain.*;
 
 import javax.persistence.*;
 
@@ -29,37 +26,57 @@ class PlannedTransaction implements IPlannedTransaction {
     }
 
     PlannedTransaction(String narration, Amount amount, Account debitor, Account creditor, Schedule schedule) {
-        Validate.notNull(debitor, "The debitor account must not be null");
-        Validate.notNull(creditor, "The creditor account must not be null");
-        Validate.notNull(schedule, "The schedule must not be null");
-        Validate.notNull(narration, "The narration must not be null");
-        Validate.notNull(amount, "The amount must not be null");
-
-        this.narration = narration;
-        this.amount = amount;
-        this.debitor = debitor;
-        this.creditor = creditor;
-        this.schedule = schedule;
+        setNarration(narration);
+        setAmount(amount);
+        setDebitor(debitor);
+        setCreditor(creditor);
+        setSchedule(schedule);
     }
 
     public Schedule getSchedule() {
         return schedule;
     }
 
+    public void setSchedule(Schedule schedule) {
+        Validate.notNull(schedule, "The schedule must not be null");
+        this.schedule = schedule;
+    }
+
     public String getNarration() {
         return narration;
+    }
+
+    public void setNarration(String narration) {
+        Validate.notNull(narration, "The narration must not be null");
+        this.narration = narration;
     }
 
     private Amount getAmount() {
         return amount;
     }
 
+    public void setAmount(Amount amount) {
+        Validate.notNull(amount, "The amount must not be null");
+        this.amount = amount;
+
+    }
+
     public Account getDebitor() {
         return debitor;
     }
 
+    public void setDebitor(IAccount debitor) {
+        Validate.notNull(debitor, "The debitor account must not be null");
+        this.debitor = (Account) debitor;
+    }
+
     public Account getCreditor() {
         return creditor;
+    }
+
+    public void setCreditor(IAccount creditor) {
+        Validate.notNull(creditor, "The creditor account must not be null");
+        this.creditor = (Account) creditor;
     }
 
     public Long getId() {
--- src/main/java/org/book/account/domain/IPlannedTransaction.java
+++ src/main/java/org/book/account/domain/IPlannedTransaction.java
@@ -3,13 +3,23 @@ package org.book.account.domain;
 public interface IPlannedTransaction {
     String getNarration();
 
+    void setNarration(String narration);
+
     IAccount getDebitor();
+
+    void setDebitor(IAccount debitor);
 
     IAccount getCreditor();
 
+    void setCreditor(IAccount creditor);
+
     Schedule getSchedule();
+
+    void setSchedule(Schedule schedule);
 
     Amount forecast(Period aPeriod);
 
     Long getId();
+
+    void setAmount(Amount amount);
 }
